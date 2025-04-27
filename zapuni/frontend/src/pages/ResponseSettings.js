1bb420f596a8a617e44733b9a031f803f9e3c570
sinhamoca <isaacofc2@gmail.com>
2025-04-27 11:24:42 -0300

    Delete zapuni directory

--- zapuni/frontend/src/pages/ResponseSettings.js
+++
@@ -1,9 +0,0 @@
-// src/pages/ResponseSettings.js
-import React from 'react';
-import ResponseSettingsComponent from '../components/ResponseSettings';
-
-const ResponseSettingsPage = () => {
-  return <ResponseSettingsComponent />;
-};
-
-export default ResponseSettingsPage;
